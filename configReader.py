c92548299af970620127f7282752d34fc95c2a16
Paweł Runowski <prunowski@kartgis.com.pl>
2020-04-05 00:49:23 +0200

    emulating switch-case with readingDict

--- configReader.py
+++ configReader.py
@@ -5,57 +5,66 @@ def init():
     global repos
     global position
     global commitOutput
+    global readingDict
     delay = 0.25
     logins = {}
     waitTimes = {}
     repos = {}
     position = (0,0)
     commitOutput = "f'{name}.git/{c.hexsha}'"
+    readingDict = {'position' : readPosition,
+                   'delay' : readDelay,
+                   'commitOutput' : readCommitOutput,
+                   'repo' : readRepo,
+                   'key' : readKey}
 
 def readConfig():
+    global readingDict
     with open('config.txt', 'r') as conf:
         for line in conf.readlines():
-            if line.startswith('position:'):
-                s = line.split('position:', 1)[1].strip('\n')
-                ss = s.split(',')
-                global position
-                position = int(ss[0]), int(ss[1])
-            elif line.startswith('delay:'):
-                s = line.split('delay:',1)[1]
-                global delay
-                delay = float(s)
-            elif line.startswith('commitOutput:'):
-                s = line.split('commitOutput:', 1)[1].strip('\n')
-                global commitOutput
-                commitOutput = s
-            elif line.startswith('repo:'):
-                s = line.split('repo:',1)[1].strip('\n')
-                ss = s.split('::')
-                name = ss[0]
-                addr = ss[1]
-                branch = 'master'
-                if len(ss) > 2:
-                    branch = ss[2]
-                global repos
-                repos[addr] = name
-            elif line.startswith('key:'):
-                s = line.split('key:',1)[1].strip('\n')
-                ss = s.split(':')
-                if len(ss) < 4:
-                    continue
-                key = ss[0]
-                login = ss[1]
-                password = ss[3]
-                waitTime = 0.0
-                if len(ss[2]) > 0:
-                    waitTime = float(ss[2])
-                global logins
-                if key in logins.keys() or key == 'p':
-                    continue
-                logins[key] = (login, password)
-                global waitTimes
-                waitTimes[key] = waitTime
+            readingDict.get(line.split(':', 1)[0], lambda x: None)(line)
 
+def readPosition(line):
+    s = line.split('position:', 1)[1].strip('\n')
+    ss = s.split(',')
+    global position
+    position = int(ss[0]), int(ss[1])
+
+def readDelay(line):
+    s = line.split('delay:',1)[1].strip('\n')
+    global delay
+    delay = float(s)
+
+def readCommitOutput(line):
+    s = line.split('commitOutput:', 1)[1].strip('\n')
+    global commitOutput
+    commitOutput = s
+
+def readRepo(line):
+    s = line.split('repo:',1)[1].strip('\n')
+    ss = s.split('::')
+    name = ss[0]
+    addr = ss[1]
+    global repos
+    repos[addr] = name
+
+def readKey(line):
+    s = line.split('key:',1)[1].strip('\n')
+    ss = s.split(':')
+    if len(ss) < 4:
+        return
+    key = ss[0]
+    login = ss[1]
+    password = ss[3]
+    waitTime = 0.0
+    if len(ss[2]) > 0:
+        waitTime = float(ss[2])
+    global logins
+    if key in logins.keys() or key == 'p':
+        return
+    logins[key] = (login, password)
+    global waitTimes
+    waitTimes[key] = waitTime
 
 def setLoginPos(x, y):
     with open('config.txt', 'r') as conf:
